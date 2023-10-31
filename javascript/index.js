c6b9d7d4964ed41031f6c9ce74b10d441b135061
David Sanchez Valera <chirlo14@gmail.com>
2023-10-31 09:19:42 +0100

    Bonus 1

--- javascript/index.js
+++ javascript/index.js
@@ -33,29 +33,52 @@ getInstruction(
   0,
   (step1) => {
     document.querySelector("#mashedPotatoes").innerHTML += `<li>${step1}</li>`;
-    getInstruction("mashedPotatoes", 1, (step2) => {
-      document.querySelector(
-        "#mashedPotatoes"
-      ).innerHTML += `<li>${step2}</li>`;
-      getInstruction("mashedPotatoes", 2, (step3) => {
+    getInstruction(
+      "mashedPotatoes",
+      1,
+      (step2) => {
         document.querySelector(
           "#mashedPotatoes"
-        ).innerHTML += `<li>${step3}</li>`;
-        getInstruction("mashedPotatoes", 3, (step4) => {
-          document.querySelector(
-            "#mashedPotatoes"
-          ).innerHTML += `<li>${step4}</li>`;
-          getInstruction("mashedPotatoes", 4, (step5) => {
+        ).innerHTML += `<li>${step2}</li>`;
+        getInstruction(
+          "mashedPotatoes",
+          2,
+          (step3) => {
             document.querySelector(
               "#mashedPotatoes"
-            ).innerHTML += `<li>${step5}</li>`;
-            document
-              .querySelector("#mashedPotatoesImg")
-              .removeAttribute("hidden");
-          });
-        });
-      });
-    });
+            ).innerHTML += `<li>${step3}</li>`;
+            getInstruction(
+              "mashedPotatoes",
+              3,
+              (step4) => {
+                document.querySelector(
+                  "#mashedPotatoes"
+                ).innerHTML += `<li>${step4}</li>`;
+                getInstruction(
+                  "mashedPotatoes",
+                  4,
+                  (step5) => {
+                    document.querySelector(
+                      "#mashedPotatoes"
+                    ).innerHTML += `<li>${step5}</li>`;
+                    document
+                      .querySelector("#mashedPotatoesImg")
+                      .removeAttribute("hidden");
+                    document.querySelector(
+                      "#mashedPotatoes"
+                    ).innerHTML += `<li>Todo listo</li>`;
+                  },
+                  (error) => console.log(error)
+                );
+              },
+              (error) => console.log(error)
+            );
+          },
+          (error) => console.log(error)
+        );
+      },
+      (error) => console.log(error)
+    );
   },
   (error) => console.log(error)
 );
